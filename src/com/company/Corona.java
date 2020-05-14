0801e0b5182dfb2becd869b2accdd5667a6d4864
DrPud1m <barbosamarco4@gmail.com>
2020-05-14 09:56:09 +0000

    Init

---
+++ src/com/company/Corona.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Corona {
+    public void registarCliente();
+    public void removerCliente();
+    public void autenticar();
+    public void atualizarEstimativa();
+}
