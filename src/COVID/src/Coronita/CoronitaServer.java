61499a40dcf68272b27aab78726c018cbd692d86
trasousa <trasousa@users.noreply.github.com>
2020-05-21 12:08:55 +0100

    21Maio_3

--- src/COVID/src/Coronita/CoronitaServer.java
+++ src/COVID/src/Coronita/CoronitaServer.java
@@ -1,13 +1,16 @@
 package COVID.src.Coronita;
 
+import COVID.src.Exceptions.AccountExceptions.InvalidUsername;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class CoronitaServer implements Interface {
-
+    private String answer[];
     private Socket socket;
     private Middleman dealer;
+    private Bag bag;
     private PrintWriter outServer;
 
 
@@ -16,6 +19,7 @@ public class CoronitaServer implements Interface {
             this.socket = new Socket(host, port);
             this.outServer = new PrintWriter(socket.getOutputStream());
             this.dealer = new Middleman(host ,port, socket);
+            this.bag = new Bag();
         }
 
     public void close()
@@ -26,37 +30,42 @@ public class CoronitaServer implements Interface {
     }
 
     @Override
-    public void registerAccount(String Username, String password){
+    public void registerAccount(String Username, String password) throws InvalidUsername {
         outServer.println("cr " + Username + " " + password);
         outServer.flush();
+        try {
+            answer = this.bag.getLetter();
+        }
+        catch (InvalidUsername e){
 
+        }
     }
 
     @Override
     public void authenticate(String Username, String password) {
         outServer.println("lg " + Username+ " " + password);
         outServer.flush();
+        answer = this.bag.getLetter();
     }
 
     @Override
     public void removeAccount(String Username, String password) {
         outServer.println("rm " + Username+ " " + password);
         outServer.flush();
+        answer = this.bag.getLetter();
     }
 
     @Override
     public void updateEstimate(int cases){
         outServer.println("up " + cases);
         outServer.flush();
+        answer = this.bag.getLetter();
     }
     @Override
     public void checkUsername(String Username) {
         outServer.println("ck " + Username);
         outServer.flush();
-    }
-
-    public void dealCases(){
-        this.dealer();
+        answer = this.bag.getLetter();
     }
 
 }
